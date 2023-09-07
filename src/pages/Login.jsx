636f56167a09de1df5222403969b53ec298a5748
Denis <denis@example.com>
2023-09-07 12:54:18 +0300

    auth - sad path

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { loginUser } from "../api.js";
 
 export { Login };
@@ -11,14 +11,29 @@ function Login() {
     email: "",
     password: "",
   });
+  const [status, setStatus] = useState("idle");
+  const [error, setError] = useState(null);
+
   const { hash } = useLocation();
+  const navigate = useNavigate();
   const message = hash === "#loginfirst" && "You must login first";
+  const isSubmitting = status === "submitting";
 
   async function handleSubmit(e) {
     e.preventDefault();
     console.log("login user");
-    const user = await loginUser(loginFormData);
-    console.log(user);
+    setStatus("submitting");
+    setError(null);
+    navigate(".", { replace: true });
+    let user;
+    try {
+      user = await loginUser(loginFormData);
+      console.log(user);
+    } catch (e) {
+      setError(e);
+    } finally {
+      setStatus("idle");
+    }
   }
 
   function handleChange(e) {
@@ -33,6 +48,7 @@ function Login() {
     <div className="login-container">
       <h1>Sign in to your account</h1>
       {message && <h3 className="red">{message}</h3>}
+      {error && <h3 className="red">{error.response.data.message}</h3>}
       <form onSubmit={handleSubmit} className="login-form">
         <input
           name="email"
@@ -50,7 +66,14 @@ function Login() {
           autoComplete="current-password"
           value={loginFormData.password}
         />
-        <button>Log in</button>
+        <button
+          disabled={isSubmitting}
+          style={
+            isSubmitting ? { backgroundColor: "rgb(255 140 56 / 50%)" } : {}
+          }
+        >
+          Log in
+        </button>
       </form>
     </div>
   );
