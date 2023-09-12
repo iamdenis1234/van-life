aa72fa79eba519e7899834950c2d6434807275bf
Denis <denis@example.com>
2023-09-12 17:26:20 +0300

    handle 'submitting'/'loading' state

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -9,18 +9,17 @@ export { Login };
 
 function Login() {
   console.log("Render Login");
-
   const error = useActionData();
   const { state } = useNavigation();
   const { hash } = useLocation();
   const message = hash === "#loginfirst" && "You must login first";
-  const isSubmitting = state === "submitting";
+  const isPending = state !== "idle";
 
   return (
     <div className="login-container">
       <h1>Sign in to your account</h1>
-      {message && !isSubmitting && <h3 className="red">{message}</h3>}
-      {error && !isSubmitting && <h3 className="red">{error.message}</h3>}
+      {message && !isPending && <h3 className="red">{message}</h3>}
+      {error && !isPending && <h3 className="red">{error.message}</h3>}
       <Form method="post" className="login-form" replace>
         <input
           name="email"
@@ -34,8 +33,8 @@ function Login() {
           placeholder="Password"
           autoComplete="current-password"
         />
-        <button disabled={isSubmitting}>
-          {isSubmitting ? "Logging in..." : "Log in"}
+        <button disabled={isPending}>
+          {isPending ? "Logging in..." : "Log in"}
         </button>
       </Form>
     </div>
