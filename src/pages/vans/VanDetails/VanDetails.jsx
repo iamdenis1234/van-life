6a51485f4a92d13694d4502292c5fabf9041b14f
Denis <denis@example.com>
2023-09-14 12:11:09 +0300

    add defer/await for /vans/:id route

--- src/pages/vans/VanDetails/VanDetails.jsx
+++ src/pages/vans/VanDetails/VanDetails.jsx
@@ -1,20 +1,20 @@
-import { Link, useLoaderData, useLocation } from "react-router-dom";
+import { Suspense } from "react";
+import { Await, Link, useLoaderData, useLocation } from "react-router-dom";
 
 export { VanDetails };
 
 function VanDetails() {
   console.log("Render VanDetails");
 
-  const van = useLoaderData();
+  const { vanPromise } = useLoaderData();
   const { state } = useLocation();
   const search = state?.search || "";
   const type = state?.type || "all";
 
-  return (
-    <div className="van-detail-container">
-      <Link to={`..?${search}`} relative="path" className="back-button">
-        &larr; <span>Back to {type} vans</span>
-      </Link>
+  function renderVan(van) {
+    console.log("renderVan function");
+
+    return (
       <div className="van-detail">
         <img src={van.imageUrl} />
         <i className={`van-type ${van.type} selected`}>{van.type}</i>
@@ -25,6 +25,17 @@ function VanDetails() {
         <p>{van.description}</p>
         <button className="link-button">Rent this van</button>
       </div>
+    );
+  }
+
+  return (
+    <div className="van-detail-container">
+      <Link to={`..?${search}`} relative="path" className="back-button">
+        &larr; <span>Back to {type} vans</span>
+      </Link>
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <Await resolve={vanPromise}>{renderVan}</Await>
+      </Suspense>
     </div>
   );
 }
--- src/pages/vans/VanDetails/loader.js
+++ src/pages/vans/VanDetails/loader.js
@@ -1,11 +1,11 @@
+import { defer } from "react-router-dom";
 import { getVans } from "../../../api.js";
 
 export { loader };
 
-async function loader({ params }) {
+function loader({ params }) {
   console.log("start VanDetails loader");
-  const vans = await getVans(params.id);
+  const vanPromise = getVans(params.id);
   console.log("end VanDetails loader");
-
-  return vans;
+  return defer({ vanPromise });
 }
