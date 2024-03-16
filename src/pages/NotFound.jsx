d56c2c2d06ac2a754b7ec99067b414b1b8a5ebd0
Denis <denis@example.com>
2024-03-16 10:52:32 +0200

    integrate MUI for not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,8 @@
+import { styled, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import { CustomButton } from "../components/CustomButton.jsx";
+import { CustomContainer } from "../components/CustomContainer.jsx";
+import { section } from "../mixins.js";
 
 export { NotFound };
 
@@ -6,11 +10,20 @@ function NotFound() {
   console.log("Render NotFound");
 
   return (
-    <div className="not-found-container">
-      <h1>Sorry, the page you were looking for was not found.</h1>
-      <Link to="/" className="link-button">
+    <Container>
+      <Title variant="h2" component="h1">
+        Sorry, the page you were looking for was not found.
+      </Title>
+      <CustomButton fullWidth size="large" component={Link} to="/">
         Return to Home
-      </Link>
-    </div>
+      </CustomButton>
+    </Container>
   );
 }
+
+const Container = styled(CustomContainer)(section, {});
+
+const Title = styled(Typography)(({ theme }) => ({
+  textAlign: "center",
+  marginBottom: theme.spacing(2),
+}));
