ad40e9665cc445b3dcb1db9d0b888723208112fd
Denis <denis@example.com>
2024-03-18 13:28:54 +0200

    make footer at the bottom of the viewport

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,23 @@
-import { styled } from "@mui/material";
+import { styled, Typography } from "@mui/material";
 
 export { Footer };
 
 function Footer() {
   console.log("Render Footer");
 
-  return <StyledFooter>&#169; 2022 #VANLIFE</StyledFooter>;
+  // TODO: add a link to the github project with a github icon
+  return (
+    <StyledFooter>
+      <Typography>&#169; 2024 #VANLIFE</Typography>
+    </StyledFooter>
+  );
 }
 
 const StyledFooter = styled("footer")(({ theme }) => ({
-  marginBlock: theme.spacing(6, 4),
+  backgroundColor: theme.palette.text.primary,
+  color: theme.palette.text.white,
+  paddingBlock: theme.spacing(3),
+  display: "flex",
+  justifyContent: "center",
+  marginTop: "auto",
 }));
