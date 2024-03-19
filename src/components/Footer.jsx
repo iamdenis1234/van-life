54100103e8e0ddab69b991b2b46d33387c5c3dd9
Denis <denis@example.com>
2024-03-19 14:28:49 +0200

    add top margin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,16 +8,22 @@ function Footer() {
   // TODO: add a link to the github project with a github icon
   return (
     <StyledFooter>
-      <Typography>&#169; 2024 #VANLIFE</Typography>
+      <Content>
+        <Typography>&#169; 2024 #VANLIFE</Typography>
+      </Content>
     </StyledFooter>
   );
 }
 
-const StyledFooter = styled("footer")(({ theme }) => ({
+const StyledFooter = styled("footer")({
+  marginTop: "auto",
+});
+
+const Content = styled("div")(({ theme }) => ({
   backgroundColor: theme.palette.text.primary,
   color: theme.palette.text.white,
+  marginTop: theme.spacing(6),
   paddingBlock: theme.spacing(3),
   display: "flex",
   justifyContent: "center",
-  marginTop: "auto",
 }));
