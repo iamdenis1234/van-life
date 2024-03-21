3e58f45f37405475840f7626430364693edc649b
Denis <denis@example.com>
2024-03-21 11:03:50 +0200

    under development message for about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,6 @@
-import { Link } from "react-router-dom";
+import { Alert, styled } from "@mui/material";
+import { CustomContainer } from "../components/CustomContainer.jsx";
+import { section } from "../mixins.js";
 
 export { About };
 
@@ -7,31 +9,40 @@ export { About };
 function About() {
   console.log("Render About");
 
+  /*
+    return (
+      <div className="about-page-container">
+        <img src="/assets/images/about-hero.png" className="about-hero-image" />
+        <div className="about-page-content">
+          <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+          <p>
+            Our mission is to enliven your road trip with the perfect travel van
+            rental. Our vans are recertified before each trip to ensure your
+            travel plans can go off without a hitch. (Hitch costs extra 😉)
+          </p>
+          <p>
+            Our team is full of vanlife enthusiasts who know firsthand the magic
+            of touring the world on 4 wheels.
+          </p>
+        </div>
+        <div className="about-page-cta">
+          <h2>
+            Your destination is waiting.
+            <br />
+            Your van is ready.
+          </h2>
+          <Link className="link-button" to="/vans">
+            Explore our vans
+          </Link>
+        </div>
+      </div>
+    );
+  */
   return (
-    <div className="about-page-container">
-      <img src="/assets/images/about-hero.png" className="about-hero-image" />
-      <div className="about-page-content">
-        <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
-        <p>
-          Our mission is to enliven your road trip with the perfect travel van
-          rental. Our vans are recertified before each trip to ensure your
-          travel plans can go off without a hitch. (Hitch costs extra 😉)
-        </p>
-        <p>
-          Our team is full of vanlife enthusiasts who know firsthand the magic
-          of touring the world on 4 wheels.
-        </p>
-      </div>
-      <div className="about-page-cta">
-        <h2>
-          Your destination is waiting.
-          <br />
-          Your van is ready.
-        </h2>
-        <Link className="link-button" to="/vans">
-          Explore our vans
-        </Link>
-      </div>
-    </div>
+    <Container>
+      <Alert severity="info">Page Under Development</Alert>
+    </Container>
   );
 }
+
+const Container = styled(CustomContainer)(section, {});
