7f60d9e6a6788358f852ee9809051268fa6b918c
Denis <denis@example.com>
2024-03-23 10:49:39 +0200

    add responsiveness to not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -14,16 +14,23 @@ function NotFound() {
       <Title variant="h2" component="h1">
         Sorry, the page you were looking for was not found.
       </Title>
-      <CustomButton fullWidth size="large" component={Link} to="/">
+      <ReturnHome size="large" component={Link} to="/">
         Return to Home
-      </CustomButton>
+      </ReturnHome>
     </Container>
   );
 }
 
-const Container = styled(CustomContainer)(section, {});
+const Container = styled(CustomContainer)(section, {
+  textAlign: "center",
+});
 
 const Title = styled(Typography)(({ theme }) => ({
-  textAlign: "center",
   marginBottom: theme.spacing(2),
+}));
+
+const ReturnHome = styled(CustomButton)(({ theme }) => ({
+  marginTop: theme.spacing(2),
+  maxWidth: 400,
+  width: "100%",
 }));
