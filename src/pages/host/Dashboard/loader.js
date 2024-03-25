4ca8351d0ddc0bd9f772dbd4275cacf3cb85f7ee
Denis <denis@example.com>
2024-03-25 11:20:38 +0200

    remove unnecessary request

--- src/pages/host/Dashboard/loader.js
+++ src/pages/host/Dashboard/loader.js
@@ -1,5 +1,3 @@
-import { defer } from "react-router-dom";
-import { getHostVans } from "../../../api.js";
 import { requireAuth } from "../../../utils/requireAuth.js";
 
 export { loader };
@@ -7,8 +5,6 @@ export { loader };
 async function loader({ request }) {
   console.log("start Dashboard loader");
   await requireAuth(request);
-  const vansPromise = getHostVans();
   console.log("end Dashboard loader");
-
-  return defer({ vansPromise });
+  return null;
 }
